25ed8e40af93dd3669ca02a59d37fefe988b7602
kienmarco2408 <kienmarco999@gmail.com>
2022-07-24 21:03:30 +0700

    update

--- src/features/screens/home/home.detail.js
+++ src/features/screens/home/home.detail.js
@@ -1,18 +1,20 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   View,
   Text,
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  Image
-} from "react-native";
-import { NewsDetail } from "../../../components/homescreen/detail/detail";
+  Image,
+} from 'react-native';
+import { NewsDetail } from '../../../components/homescreen/detail/detail';
 
 export default function DetailNews({ navigation }) {
   return (
-    <SafeAreaView>
-      <NewsDetail />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <NewsDetail />
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- src/features/screens/home/home.screen.js
+++ src/features/screens/home/home.screen.js
@@ -1,25 +1,24 @@
-import React from "react";
+import React from 'react';
 import {
   StyleSheet,
   SafeAreaView,
   Text,
   View,
-  TouchableOpacity
-} from "react-native";
-import { Button_News } from "../../../components/homescreen/button.news";
-import { Button_Tags } from "../../../components/homescreen/button.tags";
-import { Slider } from "../../../components/homescreen/slider";
-import { NewsList } from "../../../components/homescreen/news/news.list";
-import { ScrollView } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+  TouchableOpacity,
+} from 'react-native';
+import { Button_News } from '../../../components/homescreen/button.news';
+import { Button_Tags } from '../../../components/homescreen/button.tags';
+import { Slider } from '../../../components/homescreen/slider';
+import { NewsList } from '../../../components/homescreen/news/news.list';
+import { ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function HomeScreen() {
-  const navigation = useNavigation();
+export default function HomeScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.xuhuong}>
-          <Text style={{ fontSize: 24, fontWeight: "bold" }}>Xu hướng</Text>
+          <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Xu hướng</Text>
         </View>
         <View style={{ marginLeft: 10, marginBottom: 10 }}>
           <Button_Tags />
@@ -28,7 +27,7 @@ export default function HomeScreen() {
         <View style={{ marginLeft: 10, marginBottom: 10, marginTop: 10 }}>
           <Button_News />
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate("Detail News")}>
+        <TouchableOpacity onPress={() => navigation.navigate('Detail News')}>
           <NewsList />
         </TouchableOpacity>
       </ScrollView>
@@ -38,13 +37,13 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   xuhuong: {
     padding: 20,
-    paddingLeft: 16
+    paddingLeft: 16,
   },
   button_tags: {
-    borderColor: "#F5344B"
-  }
+    borderColor: '#F5344B',
+  },
 });
